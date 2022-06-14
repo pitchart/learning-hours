37d550f1c968946245f7f0d0be07cf1be45a3c75
Marie-Eugenie Mechain <MarieEugenie.mechain@cdbdx.biz>
2022-06-14 11:18:57 +0200

    add builders

--- mikado-method/test-data-builders-kata/typescript/test/com/murex/tbw/report/ReportGenerator.spec.ts
+++ mikado-method/test-data-builders-kata/typescript/test/com/murex/tbw/report/ReportGenerator.spec.ts
@@ -5,22 +5,85 @@ import { Author, EducationalBook, Category } from "../../../../../src/com/murex/
 import { Country, Currency, Language } from "../../../../../src/com/murex/tbw/domain/country";
 import { Invoice, PurchasedBook } from "../../../../../src/com/murex/tbw/purchase";
 
+class CountryBuilder{
+    
+    private name: string = "France";
+    private currency: Currency = Currency.Dollar;
+    private language: Language = Language.English;
+    
+    private constructor() {}
+
+    static ACountry() {
+        return new CountryBuilder();
+    }
+    
+    WithName(name: string) {
+        this.name = name;
+        return this;
+    }
+    
+    WithCurrency(currency: Currency) {
+        this.currency = currency;
+        return this;
+    }
+    
+    WithLanguage(language: Language) {
+        this.language = language;
+        return this;
+    }
+    
+    Build() {
+        return new Country(this.name, this.currency, this.language);
+    }
+}
+
+class AuthorBuilder{
+
+    private name: string = "Jack";
+    private country: Country = CountryBuilder.ACountry().Build();
+    
+    private constructor() {};
+    
+    static AnAuthor(): AuthorBuilder{
+        return new AuthorBuilder();
+    };
+    
+    WithName(name: string): AuthorBuilder {
+        this.name = name;
+        return this;
+    }
+    
+    WithCountry(country: Country): AuthorBuilder {
+        this.country = country;
+        return this;
+    }
+
+    Build(): Author {
+        return new Author(this.name,this.country);
+    }
+}
+
+
 describe(ReportGenerator, () => {
     it("Computes total amount without discount and without tax rate", () => {
       const repository = new InMemoryRepository();
       MainRepository.override(repository);
       const generator = new ReportGenerator();
 
-      const book = new EducationalBook(
-          "Clean Code", 25, new Author(
-              "Uncle Bob", new Country(
-                  "USA", Currency.Dollar, Language.English
-              )
-          ), Language.English, Category.COMPUTER);
+      const usa = CountryBuilder.ACountry()
+        .WithName("USA")
+        .WithCurrency(Currency.Dollar)
+        .WithLanguage(Language.English)
+        .Build();
+        
+        const bob = AuthorBuilder.AnAuthor().WithName("Uncle Bob").WithCountry(usa).Build();
 
+      const book = new EducationalBook(
+          "Clean Code", 25, bob, Language.English, Category.COMPUTER);
+        
       const purchase = new PurchasedBook(book, 2);
 
-      const invoice = new Invoice("John Doe", new Country("USA", Currency.Dollar, Language.English));
+      const invoice = new Invoice("John Doe", usa);
       invoice.addPurchasedBook(purchase);
 
       repository.addInvoice(invoice);
